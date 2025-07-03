0890ac65a4726f7d0b3aecd18379824baac2f87d
Romaxolote <romajuanmanuel@gmail.com>
2025-07-03 11:27:53 -0300

    feat: dark mode

--- src/loadPage.js
+++ src/loadPage.js
@@ -10,9 +10,34 @@ export function loadPage() {
     const footer = document.createElement('footer');
     footer.innerHTML = `<small>&copy; 2025 Mi Sitio</small>`;
 
+    const toggleTheme = document.createElement('button');
+    toggleTheme.id = 'toggleTheme';
+
+    
+
     app.append(header, main, footer);
     const weatherDiv = api();
+    header.append(toggleTheme);
     main.append(weatherDiv);
 
+    const toggleBtn = document.getElementById('toggleTheme');
+    const body = document.body;
+    const savedTheme = localStorage.getItem('theme');
+    const grid = document.getElementById('weatherContainer');
+    if (savedTheme === 'dark') {
+        body.classList.add('dark');
+        grid.classList.add('dark');
+
+        toggleBtn.textContent = '🌞';
+    } else {
+        toggleBtn.textContent = '🌙';
+    }
+
+    toggleBtn.addEventListener('click', () => {
+        const isDark = body.classList.toggle('dark') && grid.classList.toggle('dark');
+        localStorage.setItem('theme', isDark ? 'dark' : 'light');
+        toggleBtn.textContent = isDark ? '🌞' : '🌙';
+
+    });
 
 }
